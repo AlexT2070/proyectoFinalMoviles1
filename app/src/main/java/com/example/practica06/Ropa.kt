65d8be044d9ca8798b077e5bac55937ed962d56f
Oscar <a22110112@ceti.mx>
2024-12-02 15:45:37 -0600

    Creación de clase "Ropa".

---
+++ app/src/main/java/com/example/practica06/Ropa.kt
@@ -0,0 +1,11 @@
+package com.example.practica06
+
+class Ropa (
+    //Atributos
+    var nombre: String = "none",
+    var marca: String = "none",
+    var modelo: String = "none",
+    var precio: Double = 0.0,
+    var cantidad: Int = 0,
+    var descripcion: String = ""
+)
