a8f3175db498171af3dfb4566043fccf22a4280e
Oscar <a22110112@ceti.mx>
2024-12-04 18:14:27 -0600

    Validación de que no se encuentren los campos vacíos antes de dejarlo ingresar.

--- app/src/main/java/com/example/practica06/Editar_Borrar.kt
+++ app/src/main/java/com/example/practica06/Editar_Borrar.kt
@@ -1,12 +1,19 @@
 package com.example.practica06
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class Editar_Borrar : AppCompatActivity() {
+    //Instancias
+    private lateinit var Borrar: Button
+    private lateinit var Spin: Spinner
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +22,20 @@ class Editar_Borrar : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        //Asociación
+        Borrar = findViewById(R.id.btnBorrar)
+        Spin = findViewById(R.id.spinRopa)
+        //Vinculación del Spinner
+        val listado = ListadoRopa.listadoRopaCas
+        val adapter =
+            ArrayAdapter(this, android.R.layout.simple_spinner_item, listado.map { it.nombre })
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        Borrar.setOnClickListener() {
+
+            //listado.removeIf { it.nombre == "Sudadera Casual de Moda" }
+
         }
     }
 }
