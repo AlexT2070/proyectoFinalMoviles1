d6c2e75c8a62bf2a5d0cb5b79c813f10da7d3fa2
Oscar <a22110112@ceti.mx>
2024-12-04 18:13:59 -0600

    Validación de que no se encuentren los campos vacíos antes de dejarlo ingresar.

--- app/src/main/java/com/example/practica06/IngresoActivity.kt
+++ app/src/main/java/com/example/practica06/IngresoActivity.kt
@@ -50,22 +50,27 @@ class IngresoActivity : AppCompatActivity() {
             val intent = Intent(this, RegistroActivity::class.java)
             startActivity(intent)
         }
+
+
     }
 
     private fun ingresar() {
         val usuarioIngresado = usuario.text.toString()
         val contrasenaIngresada = contrasena.text.toString()
-
-        if (usuarioIngresado == "admin" && contrasenaIngresada == "12345") {
-            val intent = Intent(this, MainActivity::class.java)
-            intent.putExtra("isAdmin", true) // Usuario es administrador
-            startActivity(intent)
-        } else if (esUsuarioValido(usuarioIngresado, contrasenaIngresada)) {
-            val intent = Intent(this, MainActivity::class.java)
-            intent.putExtra("isAdmin", false) // Usuario normal
-            startActivity(intent)
-        } else {
-            Toast.makeText(this, "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show()
+        if(usuarioIngresado.isNotBlank() && contrasenaIngresada.isNotBlank()) {
+            if (usuarioIngresado == "admin" && contrasenaIngresada == "12345") {
+                val intent = Intent(this, MainActivity::class.java)
+                intent.putExtra("isAdmin", true) // Usuario es administrador
+                startActivity(intent)
+            } else if (esUsuarioValido(usuarioIngresado, contrasenaIngresada)) {
+                val intent = Intent(this, MainActivity::class.java)
+                intent.putExtra("isAdmin", false) // Usuario normal
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show()
+            }
+        }else{
+            Toast.makeText(this, "Favor de llenar los campos...", Toast.LENGTH_LONG).show()
         }
     }
 
